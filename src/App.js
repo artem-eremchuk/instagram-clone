2300395b01dbca43d2799c4925686c78534486b6
Artem Eremchuk <eremchuk.it@gmail.com>
2022-03-25 18:29:47 +0300

    add login function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.scss';
+import { createContext, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Feed from './components/Pages/Feed/Feed'
 import Login from './components/Pages/Login/Login'
 
+export const AppContext = createContext({
+  setLogin: () => {}
+})
+
 function App() {
+  const [ login, setLogin ] =  useState(false)
+
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' exact>
-          <Login />
+          <AppContext.Provider value={{setLogin}}>
+            <Login />
+          </AppContext.Provider>
         </Route>
         <Route path='/feed' exact>
-          <Feed />
+          { login && <Feed /> }
         </Route>
         <Route path='/feed/moderation' exact>
           <div>moderation</div>
--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,20 +1,57 @@
+import { useContext, useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import { AppContext } from "../../../App";
 
 const Login = () => {
-    const history = useHistory()
+    const history = useHistory();
+    const { setLogin } = useContext(AppContext);
+    const [loginValues, setLoginValues] = 
+      useState({
+        username: '', 
+        password: ''
+    });
+
+    const {username, password} = loginValues;
 
     return (
+      <div className="login-window-wrapper">
         <form onSubmit={e => {
-            e.preventDefault()
-            history.push('/feed')
+            e.preventDefault();
+
+            if (username === 'root' && password === 'root') {
+              setLogin(true)
+              history.push('/feed')
+            }
+
+            setLoginValues({
+              username: '', 
+              password: ''
+            })
+
+            document.cookie = 'sessionId=cd833f451d0d731fb2de02f0fe0deb5d; max-age=3600'
         }}>
-            <input type="text" placeholder="username" />
-            <br />
-            <input type="password" />
-            <button>
-                Log in
-            </button>
+          <input 
+            className="login-window-username"
+            value={loginValues.username}
+            onChange={(e) => setLoginValues({
+              username: e.target.value,
+              password: loginValues.password
+            })}
+            type="text" 
+            placeholder="username" 
+          />
+          <input 
+            className="login-window-password"
+            value={loginValues.password}
+            onChange={(e) => setLoginValues({
+              username: loginValues.username,
+              password: e.target.value
+            })}
+            type="password" 
+          />
+          <button>Log in</button>
         </form>
+      </div>
     )
 }
 
